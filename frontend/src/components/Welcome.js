fa15ac674bc63ce2d9068843d1e784c7d9d07fda
Aaron Polhamus <aaronpolhamus@gmail.com>
2020-05-05 19:29:36 -0500

    welcome page to set username working :)

--- frontend/src/components/Welcome.js
+++ frontend/src/components/Welcome.js
@@ -1,42 +1,42 @@
-import React, {Component} from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import { Redirect } from "react-router-dom";
 
+const Welcome = () => {
+  const [username, setUserName] = useState("")
+  const [updated, setUpdated] = useState(false)
 
-
-class Welcome extends Component {
-  constructor(props){
-    super(props);
-    this.state={'username': ''}
-  }
-
-  handleChange(e){
-    this.setState({username: e.target.value});
+  const handleChange = (e) => {
+    setUserName(e.target.value)
   }
   
-  async handleSubmit(e){
+  const handleSubmit = async (e) => { 
     e.preventDefault()
-    await axios.post("/api/set_username", {
+    const response = await axios.post("/api/set_username", {
       withCredentials: true,
-      username: this.state.username
-    }).then(
-      console.log("success")
-      // <Redirect to="/" />
-    ) 
+      username: username
+    })
+    if(response.status === 200){ 
+      setUpdated(true)
+    }
   }
-  
-  render(){
-    return (
-      <div className="modal">
-        <div className="modal_content">
-          <form>
-            <input onChange={this.handleChange.bind(this)} type="text" name="username" placeholder="Enter name here" />
-            <button onClick={this.handleSubmit.bind(this)}>Submit</button>
-          </form>
-        </div>
+
+  if (updated) { 
+    return(
+      <Redirect to="/" />
+    )
+  }
+
+  return (
+    <div className="modal">
+      <div className="modal_content">
+        <form>
+          <input onChange={handleChange} type="text" name="username" placeholder="Enter name here" />
+          <button onClick={handleSubmit}>Submit</button>
+        </form>
       </div>
-    );
-  }
- }
+    </div>
+  );
+}
 
 export default Welcome;
