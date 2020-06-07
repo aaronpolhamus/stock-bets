2d25b7cdcd0f54eb1906ddd4614f97b076163ded
Miguel Ángel Arroyo Rosales <miguel@ruidovisual.com>
2020-06-07 15:57:02 -0500

    Make AddFriend.js functionality

--- frontend/src/components/forms/AddFriends.js
+++ frontend/src/components/forms/AddFriends.js
@@ -12,11 +12,31 @@ const AddFriends = (props) => {
   const [formValues, setFormValues] = useState({});
   const [friendSuggestions, setFriendSuggestions] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [friendInvitee, setFriendInvitee] = useState("");
+
+  const [show, setShow] = useState(false);
 
   const getFriendSuggestions = async (query) => {
     const friends = await apiPost("suggest_friend_invites", { text: query });
-    console.log(query, friends);
     setFriendSuggestions(friends);
+  };
+
+  const handleClose = () => setShow(false);
+
+  const handleFriendInvite = async (e) => {
+    e.preventDefault();
+
+    if (friendInvitee !== "") {
+      const invite = await apiPost("send_friend_request", {
+        friend_invitee: friendInvitee,
+      });
+      setShow(true);
+    }
+  };
+
+  const handleChange = (e) => {
+    setFriendInvitee(e[0]);
+    console.log(friendInvitee);
   };
 
   const handleSuggestions = useCallback((query) => {
@@ -25,7 +45,7 @@ const AddFriends = (props) => {
   });
 
   return (
-    <Accordion defaultActiveKey="0">
+    <Accordion defaultActiveKey="0" className="text-right">
       <Accordion.Toggle
         as={TextButton}
         eventKey="1"
@@ -35,17 +55,43 @@ const AddFriends = (props) => {
         <Icon.PlusCircle color="var(--color-primary)" size="16" />
       </Accordion.Toggle>
       <Accordion.Collapse eventKey="1">
-        <Form.Group>
-          <AsyncTypeahead
-            id="typeahead-particpants"
-            name="invitees"
-            labelKey="name"
-            options={friendSuggestions}
-            placeholder="Who's playing?"
-            onSearch={handleSuggestions}
-          />
-        </Form.Group>
+        <Form onSubmit={handleFriendInvite}>
+          <Form.Group>
+            <AsyncTypeahead
+              id="typeahead-particpants"
+              name="invitees"
+              labelKey="name"
+              options={friendSuggestions}
+              placeholder="Who's playing?"
+              onSearch={handleSuggestions}
+              onChange={handleChange}
+            />
+          </Form.Group>
+          <Accordion.Toggle as={Button} size="sm" variant="outline-primary">
+            Close
+          </Accordion.Toggle>
+          <Button type="submit" size="sm">
+            Add friend
+          </Button>
+        </Form>
       </Accordion.Collapse>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Body>
+          <p className="text-center">
+            You've invited
+            <strong> {friendInvitee} </strong>
+            <div>to be friends with you.</div>
+            <div>
+              <small>We'll let them know!</small>
+            </div>
+          </p>
+        </Modal.Body>
+        <Modal.Footer className="centered">
+          <Button variant="primary" onClick={handleClose}>
+            Awesome!
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </Accordion>
   );
 };
