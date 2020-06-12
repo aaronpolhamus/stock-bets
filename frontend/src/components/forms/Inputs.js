2bd84286d1a6dc17148dafa217d18a6be4dae3a2
Miguel Ángel Arroyo Rosales <miguel@ruidovisual.com>
2020-06-12 10:33:27 -0500

    Add a funtion to build radiobuttons from data object

---
+++ frontend/src/components/forms/Inputs.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Form } from "react-bootstrap";
+
+//This is a helper function to render radio buttons
+//It's very important to define a name for radio buttons to work as expected and avoid collisions with other radios
+const RadioButtons = ({ options, name, onChange }) => {
+  console.log(options);
+  return null;
+};
+
+export { RadioButtons };
