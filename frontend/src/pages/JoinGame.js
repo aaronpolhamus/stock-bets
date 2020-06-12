6263ee77cce2f1ee70da9ce175cda3ad313f1665
Miguel Ángel Arroyo Rosales <miguel@ruidovisual.com>
2020-06-12 01:06:36 -0500

    Hide buttons when user accepts or declines game

--- frontend/src/pages/JoinGame.js
+++ frontend/src/pages/JoinGame.js
@@ -13,6 +13,7 @@ import { GameSettings } from "pages/game/GameSettings";
 import { PendingGameParticipants } from "pages/game/PendingGameParticipants";
 
 import { Button } from "react-bootstrap";
+import * as Icon from "react-feather";
 
 const JoinGame = (props) => {
   const { gameId } = useParams();
@@ -21,20 +22,20 @@ const JoinGame = (props) => {
   const [gameParticipants, setGameParticipants] = useState([]);
   const [invite, setInvite] = useState("");
 
+  const getGameData = async () => {
+    const gameData = await fetchGameData(gameId, "game_info");
+    setGameInfo(gameData);
+
+    const gameParticipantsData = await fetchGameData(
+      gameId,
+      "get_pending_game_info"
+    );
+    setGameParticipants(gameParticipantsData);
+  };
+
   useEffect(() => {
-    const getGameData = async () => {
-      const gameData = await fetchGameData(gameId, "game_info");
-      setGameInfo(gameData);
-
-      const gameParticipantsData = await fetchGameData(
-        gameId,
-        "get_pending_game_info"
-      );
-      setGameParticipants(gameParticipantsData);
-    };
-
     getGameData();
-  }, [gameId]);
+  }, []);
 
   const handleRespondInvite = async (decision) => {
     await apiPost("respond_to_game_invite", {
@@ -43,6 +44,45 @@ const JoinGame = (props) => {
     });
 
     setInvite(decision);
+    getGameData();
+  };
+
+  const renderButtons = (status) => {
+    switch (status) {
+      case "invited":
+        return (
+          <div>
+            <Button
+              variant="outline-danger"
+              onClick={() => {
+                handleRespondInvite("declined");
+              }}
+            >
+              Decline Invite
+            </Button>
+            <Button
+              variant="success"
+              onClick={() => {
+                handleRespondInvite("joined");
+              }}
+            >
+              Join Game
+            </Button>
+          </div>
+        );
+      case "joined":
+        return (
+          <Button variant="success" disabled>
+            Joined
+          </Button>
+        );
+      case "declined":
+        return (
+          <Button variant="danger" disabled>
+            Declined
+          </Button>
+        );
+    }
   };
 
   return (
@@ -53,30 +93,14 @@ const JoinGame = (props) => {
       <Content>
         <PageSection>
           <Breadcrumb>
-            <a href="/">&lt; Dashboard</a>
+            <a href="/">
+              <Icon.ChevronLeft size={14} style={{ marginTop: "-3px" }} />
+              Dashboard
+            </a>
           </Breadcrumb>
           <Header>
             <h1>{gameInfo.title}</h1>
-            <div>
-              <Button
-                variant="outline-secondary"
-                disabled={invite === "" ? false : true}
-                onClick={() => {
-                  handleRespondInvite("declined");
-                }}
-              >
-                {invite === "declined" ? "Declined" : "Decline"}
-              </Button>
-              <Button
-                variant="success"
-                disabled={invite === "" ? false : true}
-                onClick={() => {
-                  handleRespondInvite("joined");
-                }}
-              >
-                {invite === "joined" ? "Accepted" : "Accept Invite"}
-              </Button>
-            </div>
+            {renderButtons(gameInfo.user_status)}
           </Header>
         </PageSection>
         <PageSection>
