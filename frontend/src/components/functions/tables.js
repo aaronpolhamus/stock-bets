c8c456610830a16e4d637f9a56e63e4de1651d80
Miguel <miguel@ruidovisual.com>
2020-07-20 22:04:50 -0500

    Change AutoTable pattern to make it mobile-css friendly and accesible

--- frontend/src/components/functions/tables.js
+++ frontend/src/components/functions/tables.js
@@ -2,27 +2,45 @@ import React from 'react'
 import { Table } from 'react-bootstrap'
 import styled from 'styled-components'
 import { breakpoints } from 'design-tokens'
+import PropTypes from 'prop-types'
 
-const renderRow = (row, headers, exclude = ['order_id']) => {
+const renderRow = (row, headers, name, exclude = ['order_id']) => {
   return headers.map((key, index) => {
     if (exclude.includes(key)) {
       return null
     }
-    return <td key={index}>{row[key]}</td>
+    return (
+      <td
+        key={index}
+        headers={`${name}-${simpleTokenize(key)}`}
+      >
+        {row[key]}
+      </td>
+    )
   })
 }
 
-const makeRows = (tableData, exclude = ['order_id']) => {
+const makeRows = (tableData, name, exclude = ['order_id']) => {
+  // The name option allows us to specify unique id's for the table headers. This for accesibility and for css manipulation in responsive mode
+
   // The exclude option allows us to leave out data that we don't necessarily want represented in our table, e.g. the
   // order id for order cancellations
+
   return tableData.data.map((row, index) => {
-    return <tr key={index}>{renderRow(row, tableData.headers, exclude)}</tr>
+    return <tr key={index}>{renderRow(row, tableData.headers, name, exclude)}</tr>
   })
 }
 
-const makeHeader = (headers) => {
+const makeHeader = (headers, name) => {
   return headers.map((key, index) => {
-    return <th key={key}>{key}</th>
+    return (
+      <th
+        key={key}
+        id={`${name}-${simpleTokenize(key)}`}
+      >
+        {key}
+      </th>
+    )
   })
 }
 
@@ -48,18 +66,27 @@ const makeCustomHeader = (headers) => {
   })
 }
 
+const simpleTokenize = (string) => {
+  return string.toLowerCase().replace(/\s/g, '-')
+}
+
 const AutoTable = (props) => {
   if (props.tabledata.data) {
     return (
       <Table {...props}>
         <thead>
-          <tr>{makeHeader(props.tabledata.headers)}</tr>
+          <tr>{makeHeader(props.tabledata.headers, props.name)}</tr>
         </thead>
-        <tbody>{makeRows(props.tabledata)}</tbody>
+        <tbody>{makeRows(props.tabledata, props.name)}</tbody>
       </Table>
     )
   }
   return null
+}
+
+AutoTable.propTypes = {
+  tabledata: PropTypes.object,
+  name: PropTypes.string
 }
 
 export { AutoTable, makeHeader, makeRows, makeCustomHeader }
