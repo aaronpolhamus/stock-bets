8c2f4142b128ab9e0b707c9f6ca6d27c46be51f7
Jose <jsanchezcastillejos@gmail.com>
2020-08-16 23:25:23 -0500

    Add get method, it is necessary since not all the values stored will be jsons

--- backend/tasks/s3_cache.py
+++ backend/tasks/s3_cache.py
@@ -12,9 +12,15 @@ def set(name: str, value: str) -> None:
 
 
 def unpack_s3_json(key: str):
+    value = get(key)
+    if value:
+        return json.loads(value)
+
+
+def get(key: str):
     bucket = Config.AWS_PRIVATE_BUCKET_NAME
     s3_client = aws_client()
     if s3_client.list_objects_v2(Bucket=bucket, Prefix=f"cache/{key}")["KeyCount"] > 0:
         object_info = s3_client.get_object(Bucket=bucket, Key=f"cache/{key}")
         value = object_info['Body'].read()
-        return json.loads(value.decode())
+        return value.decode()
