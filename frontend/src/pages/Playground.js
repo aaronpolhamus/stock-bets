d2a504a58a792cd6f58883f246c0fd5e4851aa03
Miguel Arroyo Rosales <miguel@ruidovisual.com>
2020-08-17 19:31:58 -0500

    Playground

--- frontend/src/pages/Playground.js
+++ frontend/src/pages/Playground.js
@@ -8,19 +8,108 @@ import { FormattableTable } from 'components/tables/FormattableTable'
 
 const players = ['player1', 'player3', 'player3']
 const Playground = () => {
+  const gameId = 3
   return (
     <Container>
       <h1>
         Playground
-        <CustomSelect
-          name='username'
-          as='select'
-          size='sm'
-          
-        >
-          {players && players.map((element) => <option key={element} value={element}>{element}</option>)}
-        </CustomSelect>
+
       </h1>
+      <CompoundChart
+        gameId={gameId}
+        chartDataEndpoint='get_order_performance_chart'
+        legends={false}
+      >
+        {
+          ({ handleSelectedLines }) => (
+            <FormattableTable
+              hover
+              endpoint='get_fulfilled_orders_table'
+              name='orders_table'
+              gameId={gameId}
+              onRowSelect={(output) => {
+                handleSelectedLines(output)
+              }}
+              tableCellCheckbox={0}
+              formatOutput={(output) => {
+                const label = `${output.Symbol}/${output.Quantity} @ ${output['Clear price']}/${output['Cleared on']}`
+                return {
+                  label: label,
+                  color: output.color
+                }
+              }}
+              formatCells={{
+                Symbol: function renderSymbol (value, row) {
+                  return (
+                    <strong>
+                      {value}
+                    </strong>
+                  )
+                },
+                'Hypothetical return': function netChangeFormat (value) {
+                  let color = 'var(--color-text-gray)'
+                  if (parseFloat(value) < 0) {
+                    color = 'var(--color-danger)'
+                  } else if (parseFloat(value) > 0) {
+                    color = 'var(--color-success)'
+                  }
+
+                  return (
+                    <strong
+                      style={{
+                        color: color
+                      }}
+                    >
+                      {value}
+                    </strong>
+                  )
+                },
+                'Clear price': function clearPriceFormat (value, row) {
+                  const qty = row.Quantity
+                  const price = value.replace(/\$|,/g, '')
+                  const totalPrice = (qty * price).toLocaleString()
+                  return (
+                    <>
+                      <strong>
+                        {`$${totalPrice}`}
+                      </strong>
+                      <br/>
+                      <span
+                        style={{
+                          color: 'var(--color-text-gray)'
+                        }}
+                      >
+                        {`(${value})`}
+                      </span>
+                    </>
+                  )
+                }
+              }}
+              excludeRows={(row) => {
+                return row['Buy/Sell'] === 'sell'
+              }}
+              exclude={[
+                'as of',
+                'Buy/Sell',
+                'Order type',
+                'Time in force',
+                'Market price',
+                'Placed on',
+                'Order price'
+              ]}
+              sortBy='Hypothetical return'
+              showColumns={{
+                md: [
+                  'Symbol',
+                  'Quantity',
+                  'Clear price',
+                  'Hypothetical return'
+                ]
+              }}
+            />
+          )
+        }
+      </CompoundChart>
     </Container>
   )
 }
