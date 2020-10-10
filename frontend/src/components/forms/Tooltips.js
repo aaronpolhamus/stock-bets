b7f98e8fbbbfec0a884fc38bf48d469ab3d62cf1
Miguel Arroyo Rosales <miguel@ruidovisual.com>
2020-10-09 22:11:10 -0500

    Add timeout functionality to Tooltip.js

--- frontend/src/components/forms/Tooltips.js
+++ frontend/src/components/forms/Tooltips.js
@@ -3,21 +3,40 @@ import { Popover, Overlay } from 'react-bootstrap'
 import { HelpCircle } from 'react-feather'
 import PropTypes from 'prop-types'
 
-const Tooltip = ({ message }) => {
+const Tooltip = ({ message, showDelay = 300, hideDelay = 300 }) => {
   const [show, setShow] = useState(false)
   const target = useRef(null)
+  let hideTimeout = null
+  let showTimeout = null
+
+  const handleMouseEnter = () => {
+    clearTimeout(hideTimeout)
+    if (!show) {
+      showTimeout = setTimeout(() => {
+        setShow(true)
+      }, showDelay)
+    }
+  }
+
+  const handleMouseOut = () => {
+    clearTimeout(showTimeout)
+    if (show) {
+      hideTimeout = setTimeout(() => {
+        setShow(false)
+      }, hideDelay)
+    }
+  }
   return (
     <>
       <HelpCircle
         size={14}
-        style={{ marginTop: '2px' }}
+        style={{ marginTop: '2px', cursor: 'pointer'}}
         ref={target}
-        onMouseEnter={() => {
-          setShow(true)
+        onClick={() => {
+          setShow(!show)
         }}
-        onMouseLeave={() => {
-          setShow(false)
-        }}
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseOut}
       />
       <Overlay
         placement='auto'
@@ -31,12 +50,8 @@ const Tooltip = ({ message }) => {
         {({ ...props }) => (
           <Popover
             {...props}
-            onMouseEnter={() => {
-              setShow(true)
-            }}
-            onMouseLeave={() => {
-              setShow(false)
-            }}
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseOut}
           >
             <Popover.Content>
               {message}
@@ -52,6 +67,9 @@ Tooltip.propTypes = {
   message: PropTypes.oneOfType([
     PropTypes.string,
     PropTypes.node
-  ])
+  ]),
+  showDelay: PropTypes.number,
+  hideDelay: PropTypes.number
+
 }
 export { Tooltip }
