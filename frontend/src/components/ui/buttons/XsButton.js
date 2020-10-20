f744527c80148e5414839338a91b7a3a9f682a2c
Miguel Arroyo Rosales <miguel@ruidovisual.com>
2020-10-20 17:31:30 -0500

    Reorganize games in home

---
+++ frontend/src/components/ui/buttons/XsButton.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+const XsButton = styled.button`
+  font-size: var(--font-size-min);
+  appearance: none;
+  background: transparent;
+  border: 1px solid var(--color-cool-gray);
+  margin-right: var(--space-50);
+  border-radius: var(--space-200);
+`
+
+export { XsButton }
